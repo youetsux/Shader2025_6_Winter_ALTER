43a5a22ad3bbdc9d07786a018582424addb5bb19
youetsux@gmail.com <youetsux@gmail.com>
2026-02-09 02:02:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Stage.cpp
@@ -0,0 +1,156 @@
+#include "Stage.h"
+#include <string>
+#include <vector>
+#include "Engine//Model.h"
+#include "resource.h"
+#include <cassert>
+#include "Engine/camera.h"
+#include "Engine/Input.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx11.h"
+#include "imgui/imgui_impl_win32.h"
+
+
+
+Stage::Stage(GameObject* parent)
+	:GameObject(parent, "Stage"),  pConstantBuffer_(nullptr)
+{
+	hball_ = -1;
+	hRoom_ = -1;
+	hGround_ = -1;
+	hDonut_ = -1;
+
+	
+}
+
+Stage::~Stage()
+{
+}
+
+
+void Stage::InitConstantBuffer()
+{
+	D3D11_BUFFER_DESC cb;
+	cb.ByteWidth = sizeof(CONSTANTBUFFER_STAGE);
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.MiscFlags = 0;
+	cb.StructureByteStride = 0;
+
+	// コンスタントバッファの作成
+	HRESULT hr;
+	hr = Direct3D::pDevice->CreateBuffer(&cb, nullptr, &pConstantBuffer_);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, L"コンスタントバッファの作成に失敗しました", L"エラー", MB_OK);
+	}
+}
+
+void Stage::Initialize()
+{
+	InitConstantBuffer();
+	hball_ = Model::Load("ball.fbx");
+	assert(hball_ >= 0);
+	hRoom_ = Model::Load("room.fbx");
+	assert(hRoom_ >= 0);
+	hGround_ = Model::Load("plane3.fbx");
+	assert(hGround_ >= 0);
+	hDonut_ = Model::Load("donut_phong.fbx");
+	assert(hDonut_ >= 0);
+	//pMelbourne_ = new Sprite(L"Assets\\melbourne.png");
+	Camera::SetPosition({ 0, 0.8, -2.8 });
+	Camera::SetTarget({ 0,0.8,0 });
+}
+
+void Stage::Update()
+{
+    transform_.rotate_.y += 0.5f;
+    if (Input::IsKey(DIK_A))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x - 0.01f,p.y, p.z,p.w };
+        Direct3D::SetLightPos(p);
+    }
+    if (Input::IsKey(DIK_D))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x + 0.01f,p.y, p.z,p.w };
+        Direct3D::SetLightPos(p);
+    }
+    if (Input::IsKey(DIK_W))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x,p.y, p.z + 0.01f,p.w };
+        Direct3D::SetLightPos(p);
+    }
+    if (Input::IsKey(DIK_S))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x ,p.y, p.z - 0.01f,p.w };
+        Direct3D::SetLightPos(p);
+    }
+    if (Input::IsKey(DIK_UP))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x,p.y + 0.01f, p.z,p.w };
+        Direct3D::SetLightPos(p);
+    }
+    if (Input::IsKey(DIK_DOWN))
+    {
+        XMFLOAT4 p = Direct3D::GetLightPos();
+        p = { p.x ,p.y - 0.01f, p.z,p.w };
+        Direct3D::SetLightPos(p);
+    }
+
+    //コンスタントバッファの設定と、シェーダーへのコンスタントバッファのセットを書くよ
+    CONSTANTBUFFER_STAGE cb;
+    cb.lightPosition = Direct3D::GetLightPos();
+    XMStoreFloat4(&cb.eyePosition, Camera::GetPosition());
+
+    D3D11_MAPPED_SUBRESOURCE pdata;
+    Direct3D::pContext->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	// GPUからのデータアクセスを止める
+    memcpy_s(pdata.pData, pdata.RowPitch, (void*)(&cb), sizeof(cb));	// データを値を送る
+    Direct3D::pContext->Unmap(pConstantBuffer_, 0);	//再開
+
+    //コンスタントバッファ
+    Direct3D::pContext->VSSetConstantBuffers(1, 1, &pConstantBuffer_);	//頂点シェーダー用	
+    Direct3D::pContext->PSSetConstantBuffers(1, 1, &pConstantBuffer_);	//ピクセルシェーダー用
+}
+
+void Stage::Draw()
+{
+    Transform ltr;
+    ltr.position_ = { Direct3D::GetLightPos().x,Direct3D::GetLightPos().y,Direct3D::GetLightPos().z };
+    ltr.scale_ = { 0.1,0.1,0.1 };
+    Model::SetTransform(hball_, ltr);
+    Model::Draw(hball_);
+
+
+    Transform tr;
+    tr.position_ = { 0, 0, 0 };
+    //tr.scale_ = { 5.0f, 5.0f, 5.0f };
+    tr.rotate_ = { 0, 180, 0 };
+    //Model::SetTransform(hGround, tr);
+    //Model::Draw(hGround);
+
+    Model::SetTransform(hRoom_, tr);
+    Model::Draw(hRoom_);
+
+    static Transform tDonut;
+    tDonut.scale_ = { 0.25,0.25,0.25 };
+    tDonut.position_ = { 0, 0.5, 0 };
+    tDonut.rotate_.y += 0.1;
+    Model::SetTransform(hDonut_, tDonut);
+    Model::Draw(hDonut_);
+
+	ImGui::Text("Stage Class rot:%lf", tDonut.rotate_.z);
+}
+
+void Stage::Release()
+{
+}
+
+
+
---
+++ Stage.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Engine\\GameObject.h"
+#include <windows.h>
+#include "Engine\\Sprite.h"
+
+namespace
+{
+}
+struct CONSTANTBUFFER_STAGE
+{
+	XMFLOAT4 lightPosition;// 光源の位置(らんばーとのときは方向）
+	XMFLOAT4 eyePosition; // カメラの位置
+};
+
+class Stage :
+    public GameObject
+{
+public:
+	Stage(GameObject *parent);
+	~Stage();
+	void Initialize() override;
+	void Update() override;
+	void Draw()override;
+	void Release()override;
+private:
+	int hball_;    //モデル番号
+	int hRoom_;
+	int hGround_;
+	int hDonut_;
+	//Sprite* pMelbourne_;
+	ID3D11Buffer* pConstantBuffer_;
+	void InitConstantBuffer();
+};
+
